39c93604e5a0316611c80ee062737f2f950cfb4b
Joao Paulo Alencar Rodrigues <joaopauloalencarrodrigues@gmail.com>
2024-03-08 19:18:25 -0300

    feat(render): extracts render function and improves loop code

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,31 @@
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::render::WindowCanvas;
 use std::time::Duration;
 
+fn render(canvas: &mut WindowCanvas, color: Color) {
+    canvas.set_draw_color(color);
+    canvas.clear();
+    canvas.present();
+}
+
 fn main() -> Result<(), String> {
+
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
 
-    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+    let window = video_subsystem.window("Sprite Game", 800, 600)
         .position_centered()
         .build()
         .expect("could not initialize video subsystem");
 
     let mut canvas = window.into_canvas().build().expect("could not make a canvas");
 
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
     'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
+        // Game events
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} |
@@ -31,10 +34,15 @@ fn main() -> Result<(), String> {
                 },
                 _ => {}
             }
-        // The rest of the game loop goes here...
         }
 
-        canvas.present();
+        // Update
+        i = (i + 1) % 255;
+
+        // Render
+        render(&mut canvas, Color::RGB(i, 68, 255 - i));
+
+        // Time management
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 
