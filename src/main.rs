c2f2955820f00b14037dbfb878157fbca11387a2
Joao Paulo Alencar Rodrigues <joaopauloalencarrodrigues@gmail.com>
2024-03-09 00:01:40 -0300

    feat(image): adds sdl2 image renderer and basic image rendering implementation

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
+use sdl2::image::{InitFlag, LoadTexture};
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2::render::WindowCanvas;
+use sdl2::render::{Texture, WindowCanvas};
+use std::env;
+use std::path::Path;
 use std::time::Duration;
 
-fn render(canvas: &mut WindowCanvas, color: Color) {
+fn render(canvas: &mut WindowCanvas, color: Color, texture: &Texture) -> Result<(), String> {
     canvas.set_draw_color(color);
     canvas.clear();
+
+    canvas.copy(texture, None, None)?;
+
     canvas.present();
+    
+    Ok(())
 }
 
 fn main() -> Result<(), String> {
-
     let sdl_context = sdl2::init()?;
+    let _image_context = sdl2::image::init(InitFlag::PNG | InitFlag::JPG)?;
     let video_subsystem = sdl_context.video()?;
 
     let window = video_subsystem.window("Sprite Game", 800, 600)
@@ -21,6 +29,10 @@ fn main() -> Result<(), String> {
         .expect("could not initialize video subsystem");
 
     let mut canvas = window.into_canvas().build().expect("could not make a canvas");
+
+    let args: Vec<_> = env::args().collect();
+    let texture_creator = canvas.texture_creator();
+    let texture = texture_creator.load_texture(Path::new(&args[1]))?;
 
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
@@ -40,7 +52,7 @@ fn main() -> Result<(), String> {
         i = (i + 1) % 255;
 
         // Render
-        render(&mut canvas, Color::RGB(i, 68, 255 - i));
+        render(&mut canvas, Color::RGB(i, 68, 255 - i), &texture)?;
 
         // Time management
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
