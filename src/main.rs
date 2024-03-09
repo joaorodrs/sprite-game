091159def59be7ca5023874569339f229801231c
Joao Paulo Alencar Rodrigues <joaopauloalencarrodrigues@gmail.com>
2024-03-09 00:22:50 -0300

    feat(image): improves render fn and centers image rect

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,26 @@ use sdl2::image::{InitFlag, LoadTexture};
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::rect::{Point, Rect};
 use sdl2::render::{Texture, WindowCanvas};
-use std::env;
 use std::path::Path;
 use std::time::Duration;
 
-fn render(canvas: &mut WindowCanvas, color: Color, texture: &Texture) -> Result<(), String> {
+fn render(
+    canvas: &mut WindowCanvas,
+    color: Color,
+    texture: &Texture,
+    position: Point,
+    sprite: Rect
+) -> Result<(), String> {
     canvas.set_draw_color(color);
     canvas.clear();
 
-    canvas.copy(texture, None, None)?;
+    let (width, height) = canvas.output_size()?;
+
+    let screen_position = position + Point::new(width as i32 / 2, height as i32 / 2);
+    let screen_rect = Rect::from_center(screen_position, sprite.width(), sprite.height());
+    canvas.copy(texture, sprite, screen_rect)?;
 
     canvas.present();
     
@@ -30,9 +40,12 @@ fn main() -> Result<(), String> {
 
     let mut canvas = window.into_canvas().build().expect("could not make a canvas");
 
-    let args: Vec<_> = env::args().collect();
     let texture_creator = canvas.texture_creator();
-    let texture = texture_creator.load_texture(Path::new(&args[1]))?;
+    let texture = texture_creator.load_texture(Path::new("assets/bardo.png"))?;
+
+    let position = Point::new(0, 0);
+    // Source position in the spritesheet
+    let sprite = Rect::new(0, 0, 26, 36);
 
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
@@ -52,7 +65,7 @@ fn main() -> Result<(), String> {
         i = (i + 1) % 255;
 
         // Render
-        render(&mut canvas, Color::RGB(i, 68, 255 - i), &texture)?;
+        render(&mut canvas, Color::RGB(i, 68, 255 - i), &texture, position, sprite)?;
 
         // Time management
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
