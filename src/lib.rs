2849189e52b9128afb43913c4663ff6deb08f75e
Joao Paulo Alencar Rodrigues <joaopauloalencarrodrigues@gmail.com>
2024-03-12 22:28:53 -0300

    feat(lib): adds lib components

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,41 @@ use std::collections::VecDeque;
 
 use sdl2::rect::{Point, Rect};
 
+use specs::prelude::*;
+use specs_derive::Component;
+
 pub const PLAYER_MOVEMENT_SPEED: i32 = 5;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Direction { Up, Down, Left, Right }
+
+#[derive(Component, Debug)]
+#[storage(VecStorage)]
+pub struct Position(Point);
+
+#[derive(Component, Debug)]
+#[storage(VecStorage)]
+pub struct Velocity {
+    pub speed: i32,
+    pub direction: Direction,
+}
+
+#[derive(Component, Debug)]
+#[storage(VecStorage)]
+pub struct Sprite {
+    pub spritesheet: usize,
+    pub region: Rect,
+}
+
+#[derive(Component, Debug)]
+#[storage(VecStorage)]
+pub struct MovementAnimation {
+    pub current_frame: usize,
+    pub up_frames: Vec<Sprite>,
+    pub down_frames: Vec<Sprite>,
+    pub left_frames: Vec<Sprite>,
+    pub right_frames: Vec<Sprite>,
+}
 
 #[derive(Debug)]
 pub struct Player {
